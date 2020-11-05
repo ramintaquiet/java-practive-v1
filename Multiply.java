079bdb0c0ab9b6f05949a7a576e98715f8578d3d
Raminta <56090053+ramintaquiet@users.noreply.github.com>
2020-11-05 11:50:01 +0000

    one

---
+++ Multiply.java
@@ -0,0 +1,31 @@
+
+
+public class Multiply {
+
+ public static void main(String args[]) {
+
+  double a=3.12;
+  double b=200;
+  double res;
+
+     res = multi(a, b); 
+
+
+     System.out.println("method inside ");
+
+  System.out.println("the result is " + res);
+
+
+ }
+
+  
+    public static double multi(double a, double b) {
+        return a * b;
+      }
+    
+
+ 
+
+
+      
+    }
